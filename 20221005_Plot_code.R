87acab4cee0a5fb002e2191402a592ccb005f740
cccarrillo <79611699+cccarrillo@users.noreply.github.com>
2023-07-25 16:27:29 -0500

    Carra pushing the latest changes to the code

--- 20221005_Plot_code.R
+++ 20221005_Plot_code.R
@@ -17,13 +17,13 @@ dat %>%
   scale_y_continuous(limits = c(0,350), expand = c(0,0), breaks = seq(0, 350, 50)) +
   geom_ribbon(
     data = ~ pivot_wider(., names_from = RunNumber, values_from = infested),
-    aes(ymin = Run5, ymax = Run1), fill = "blue", alpha = 0.25
+    aes(ymin = Run6, ymax = Run10), fill = "blue", alpha = 0.25
   ) +
   geom_line(aes(y = infested,
                 linetype=RunNumber), # added line type back inside "aes"
             color= "black", # added color outside "aes" so all would be black
             lwd  = 0.5, )  +
-  scale_linetype_manual(name = "", values = c("dashed", "solid", "dotted")) + 
+  scale_linetype_manual(name = "", values = c("dashed", "dotted", "solid")) + 
   ylab("Number of lakes infested") + xlab("Timestep") +
   
   theme_bw() +
@@ -31,6 +31,6 @@ dat %>%
   theme(axis.text=element_text(size=12), axis.title=element_text(size=14))+ # Changes the font size of axes labels
   theme(panel.grid.minor = element_blank()) # takes out the minor grid lines -- I did this because you don't have 0.5 timesteps
 
-ggsave("Boater_Threshold_11212022.jpg", height=10, width=8)
+ggsave("HSI_12222022.jpg", height=10, width=8)
 
 
